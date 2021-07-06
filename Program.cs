ce05b03bee2c01cdc98d7b5d82c3fdbed025a361
Rodolfo Roman <rromanve@southtexascollege.edu>
2021-07-06 12:23:01 -0500

    Project Created, Stuff Class too

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace coolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+            Stuff stuff = new();
+            stuff.x = 3;
+            stuff.y = 2;
+            Console.WriteLine($"The sum is {stuff.sum()}");
+        }
+    }
+}
---
+++ Stuff.cs
@@ -0,0 +1,15 @@
+namespace coolApp
+{
+  public class Stuff
+  {
+    public int x {get; set;}
+    public int y {get; set;}
+    
+    
+
+    public int sum()
+    {
+      return x+y;
+    }
+  }
+}   
